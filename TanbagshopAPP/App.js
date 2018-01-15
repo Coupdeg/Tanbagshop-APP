35fa4c36a1a6daeea2ae6c25a6d26d1e0b1fdbe3
ntpbright <nuttapatprom.c@ku.com>
2018-01-15 10:45:00 +0700

    save

--- TanbagshopAPP/App.js
+++ TanbagshopAPP/App.js
@@ -46,9 +46,8 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    alignItems: "stretch"
   },
   header: {
     paddingTop: Platform.OS === "ios" ? 20 : 0,
