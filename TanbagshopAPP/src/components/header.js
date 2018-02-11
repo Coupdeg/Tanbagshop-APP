7eaee27a5cbce035910b4695dd20198af833d017
Thirasan <thirasan@Thirasans-MacBook-Pro.local>
2018-02-11 22:25:07 +0700

    Complete prototype app core

--- TanbagshopAPP/src/components/header.js
+++ TanbagshopAPP/src/components/header.js
@@ -1,85 +1,94 @@
 // src/components/header.js
 // Import libraries
 import React, { Component } from 'react';
-import propTypes from 'prop-types';
-import { Text, View } from 'react-native';
+import {
+  Dimensions,
+  AppRegistry,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar
+} from 'react-native';
 import { Button } from 'react-native-elements';
-import EvilIcon from 'react-native-vector-icons/EvilIcons'
-import SearchBar from 'react-native-search-bar';
+import propTypes from 'prop-types';
+import SearchHeader from 'react-native-search-header';
+import Icon from 'react-native-vector-icons/MaterialIcons'
  
 // สร้าง Component
 class Header extends React.Component {
-    showSearchBar(){
-      this.searchBar.hide()
-    }
-    render() {
-        return (
-        <View style={styles.viewStyle}>
-        <SearchBar
-          style={styles.searchStyle}
-          ref={(ref) => this.searchBar = ref}
-          data={items}
-          handleResults={this.handleResults}
-          showOnLoad
-        />
-        <Button
-          raised        
-          icon = {<EvilIcon size={24} color='black' name='search' />}
-          text = "Search"
-        />
-        {/*<Text style={styles.textStyle}>{this.props.headerText}</Text>*/}
+  constructor (props) {
+    super(props);
+  }
+  render() {
+      return (
+        <View style = { styles.container }>
+          <StatusBar barStyle = 'light-content' />
+          <View style = { styles.status }/>
+          <View style = { styles.header }>
+              <Text style = { styles.label }> Tan </Text>
+              <Icon
+                  name='search'
+                  style = {{borderRightWidth: 10}}
+                  size={24} 
+                  color = 'white' 
+                  onPress = {() => this.searchHeader.show()}/>
+          </View>
+          <SearchHeader
+              ref = {(searchHeader) => {
+                  this.searchHeader = searchHeader;
+              }}
+              onGetSearchAutocompletions = {async (text) => {
+                  if (text) {
+                      const response = await fetch(`http://suggestqueries.google.com/complete/search?client=firefox&q=${text}`, {
+                          method: `get`
+                      });
+                      const data = await response.json();
+                      return data[1];
+                  } else {
+                      return [];
+                  }
+              }}
+          />
         </View>
-        );
-    }
+      );
+  }
 }
 Header.propTypes = {
   headerText: propTypes.string.isRequired
 };
- 
-const styles = {
-  searchStyle: {
-    top: 5,
-    width: 325,
-    height: 40
+
+const DEVICE_WIDTH = Dimensions.get(`window`).width;
+
+const styles = StyleSheet.create({
+  container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#f5fcff'
   },
-  viewStyle: {
-    backgroundColor: '#F8F8F8',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    height: 80,
-    paddingTop: 30,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
+  status: {
+      zIndex: 10,
+      elevation: 2,
+      width: DEVICE_WIDTH,
+      height: 21,
+      backgroundColor: '#0097a7'
   },
-  textStyle: {
-    fontSize: 20
-    
-  }
-};
-const items = [
-  1337,
-  'janeway',
-  {
-    lots: 'of',
-    different: {
-      types: 0,
-      data: false,
-      that: {
-        can: {
-          be: {
-            quite: {
-              complex: {
-                hidden: [ 'gold!' ],
-              },
-            },
-          },
-        },
-      },
-    },
+  header: {
+      justifyContent: 'space-between',
+      flexDirection: 'row',
+      alignItems: 'center',
+      width: DEVICE_WIDTH,
+      height: 56,
+      marginBottom: 6,
+      backgroundColor: '#00bcd4'
   },
-  [ 4, 2, 'tree' ],
-];
+  label: {
+      fontSize: 20,
+      fontWeight: '600',
+      borderLeftWidth: 165,
+      color: '#f5fcff',
+      backgroundColor: 'transparent'
+  },
+});
  
 
  
